6a1e2693044041af99bd2a1f735094f5f4a691d0
Holger Dirks <h.dirks@28apps.de>
2023-04-18 15:29:01 +0200

    erster stuff

---
+++ header.php
@@ -0,0 +1,10 @@
+<?php
+header('Veranstaltungen');
+header('Veranstaltungen');
+header('Veranstaltungen');
+header('Veranstaltungen');
+header('Veranstaltungen');
+header('Veranstaltungen');
+header('Veranstaltungen');
+header('Veranstaltungen');
+?>
