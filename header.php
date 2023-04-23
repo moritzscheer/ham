a9714894cd6a7b1bcb4a927fc0bc39c8418b5e22
Holger Dirks <h.dirks@28apps.de>
2023-04-23 20:48:06 +0200

    erster entwurf bands, grid system eingefügt

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
-<?php
-header('Veranstaltungen');
-header('Veranstaltungen');
-header('Veranstaltungen');
-header('Veranstaltungen');
-header('Veranstaltungen');
-header('Veranstaltungen');
-header('Veranstaltungen');
-header('Veranstaltungen');
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Embed PHP in a .html File</title>
+</head>
+<body>
+<h1><?php echo "Hello World" ?></h1>
+</body>
+</html>
