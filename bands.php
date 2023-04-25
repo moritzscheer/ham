9283565e5e63909d3e8c5f089fe05ee6e25fe3ef
Holger Dirks <h.dirks@28apps.de>
2023-04-25 12:58:40 +0200

    bild hinzugefügt

---
+++ bands.php
@@ -0,0 +1,18 @@
+<?php include_once "php/head.php" ?>
+<body>
+<?php include_once "php/nav.php" ?>
+</nav>
+
+<div>
+    <div>
+        <img src ="band.jpg" alt="Paris"
+        width="240">
+    </div>
+    <div>
+        max
+    </div>
+</div>
+
+<?php include_once "php/footer.php" ?>
+</body>
+</html>
