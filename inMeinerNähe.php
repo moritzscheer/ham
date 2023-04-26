c31549d16e66a34e912cf78db6cd08399e685dc0
moritzscheer <moritz@die-scheers.de>
2023-04-26 10:37:17 +0200

    wadwswa

--- inMeinerNähe.php
+++ inMeinerNähe.php
@@ -2,9 +2,12 @@
 
 <body>
 
-    <!-- input -->
     <?php include_once "php/header.php" ?>
+
+    <!-- input -->
     <div>
+        <h1> In meiner Nähe</h1>
+
         <!-- Filter -->
         <div>
             <form method="searchMap" action="event.php">
@@ -20,21 +23,42 @@
         </div>
     </div>
 
-    <!-- Event 1 -->
+    <!-- Output -->
     <div>
-        <img src="images/event2.jpg" alt="eventImage"/>
-        <br>
-        <span>
+        <!-- Event 1 -->
+        <div>
+            <img src="images/event.jpg" alt="eventImage"/>
+            <br>
+            <span>
            Name: Neueröffnung Hornbach Bau- und Gartenmarkt Leipzig
         </span>
-        <br>
-        <span>
+            <br>
+            <span>
              Datum: 12 April 2023
         </span>
-        <br>
-        <span>
+            <br>
+            <span>
            Adresse: Steinweg 2, 42275 Wuppertal
         </span>
+        </div>
+        <br>
+
+        <!-- Event 1 -->
+        <div>
+            <img src="images/event2.jpg" alt="eventImage"/>
+            <br>
+            <span>
+           Name: Neueröffnung Hornbach Bau- und Gartenmarkt Leipzig
+        </span>
+            <br>
+            <span>
+             Datum: 12 April 2023
+        </span>
+            <br>
+            <span>
+           Adresse: Steinweg 2, 42275 Wuppertal
+        </span>
+        </div>
     </div>
 
     <?php include_once "php/footer.php" ?>
