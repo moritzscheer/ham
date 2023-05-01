ce788d26978c65604a9a6597eac3246054599fba
Moritz <moritz.scheer@uni-oldenburg.de>
2023-05-01 22:23:58 +0200

    wdadwad

--- php/head.php
+++ php/head.php
@@ -5,5 +5,6 @@
     <meta charset="utf-8">
     <meta name="author" content="Holger, Artur und Moritz">
     <meta name="keywords" content="Musiker, Veranstalter">
+    <link rel="stylesheet" type="text/css" href="css/format.css">
     <title>Ham</title>
 </head>
--- php/header.php
+++ php/header.php
@@ -1,33 +1,38 @@
 
 <nav>
-    <div>
-        <form action="bands.php" class="header">
-            <input type="submit" value="Bands" class="navbar-button">
-        </form>
-    </div>
-    <div>
-        <form action="events.php" class="header">
-            <input type="submit" value="Veranstaltungen" class="navbar-button">
-        </form>
-    </div>
-    <div>
-        <form action="inMeinerNähe.php" class="header">
-            <input type="submit" value="In meiner Nähe" class="navbar-button">
-        </form>
-    </div>
-    <div>
-        <form action="createEvent.php" class="header">
-            <input type="submit" value="Veranstaltung erstellen" class="navbar-button">
-        </form>
-    </div>
-    <div>
-        <form action="profil.php" class="header">
-            <input type="submit" value="Profil" class="navbar-button">
-        </form>
-    </div>
-    <div >
-        <form action="profilBearbeiten.php" class="header">
-            <input type="submit" value="Profil Bearbeiten" class="navbar-button">
-        </form>
-    </div>
+    <ul>
+        <li>
+            <form action="bands.php" class="header">
+                <input type="submit" value="Bands" class="navbar-button">
+            </form>
+        </li>
+        <li>
+            <form action="events.php" class="header">
+                <input type="submit" value="Veranstaltungen" class="navbar-button">
+            </form>
+        </li>
+        <li>
+            <form action="inMeinerNähe.php" class="header">
+                <input type="submit" value="In meiner Nähe" class="navbar-button">
+            </form>
+        </li>
+        <li>
+            <form action="createEvent.php" class="header">
+                <input type="submit" value="Veranstaltung erstellen" class="navbar-button">
+            </form>
+        </li>
+        <li class="dropdown">
+            <img src="images/profilePicture.jpg" height="30" width="30" class="profilPicture">
+
+            <div class="dropdown-content">
+                <form action="profil.php" class="header">
+                    <input type="submit" value="Profil" class="navbar-button">
+                </form>
+                <form action="index.php" class="header">
+                    <input type="submit" value="Log-out" class="navbar-button">
+                </form>
+            </div>
+
+        </li>
+    </ul>
 </nav>
