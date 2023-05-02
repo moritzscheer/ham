0ab69af5c753f4178c20a97e4e227633e4ad69cb
Holger Dirks <h.dirks@28apps.de>
2023-05-02 15:15:41 +0200

    header update

--- bands.php
+++ bands.php
@@ -4,39 +4,52 @@
 
 
 <div>
+
+
     <h1>Bands</h1>
 
     <!-- Band 1 -->
-    <div>
-
-        <img src ="images/band.jpg" alt="bandImage"/>
-        <br>
-        <span>
+    <div id="item-list-view">
+        <div id="item-list">
+            <div id="item">
+                <div>
+                    <img src="images/band.jpg" alt="bandImage"/>
+                </div>
+                <div>
+                      <span>
             Name: Chrystal Ball
         </span>
-        <br>
-        <span>
+                    <br>
+                    <span>
               Genre: Hard Rock
         </span>
-        <br>
-        <span>
+                    <br>
+                    <span>
            2 Gitarre
         </span>
-        <br>
-        <span>
+                    <br>
+                    <span>
              1 Schlagzeug
         </span>
-        <br>
-        <span>
+                    <br>
+                    <span>
             1 Bass
         </span>
-        <br>
-        <span>
+                    <br>
+                    <span>
             Gage: 1480 € pro Abend
         </span>
+                </div>
+            </div>
+        </div>
+        <div>
+            moin
+        </div>
     </div>
+
 </div>
 
 <?php include_once "php/footer.php" ?>
 </body>
 </html>
+
