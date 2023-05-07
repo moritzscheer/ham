5a5589736e2b7e202383320cb4c94793d06935d8
artur-S213 <mcdi1595@gmail.com>
2023-05-08 00:51:34 +0200

    css styling on login and register

--- Profil/login.php
+++ Profil/login.php
@@ -2,25 +2,17 @@
 
 <body>
 <div class="login">
-    <div class="signIn">
-        <form>
-            <label id="Hlogin">Login</label>
-        </form>
+    <div class="loginElement">
+        <label id="Hlogin">Login</label>
     </div>
-    <div class="signIn">
-        <form>
-            <input type="email" id="lemail" placeholder="E-Mail adresse" required>
-        </form>
+    <div class="loginElement">
+        <input type="email" id="lemail" placeholder="E-Mail adresse" required>
     </div>
-    <div class="signIn">
-        <form >
-            <input type="password" id="lpassword" placeholder="Passwort" required>
-        </form>
+    <div class="loginElement">
+        <input type="password" id="lpassword" placeholder="Passwort" required>
     </div>
-    <div class="signIn">
-        <form>
-            <input type="submit" id="loginSubmit" value="Login">
-        </form>
+    <div class="loginElement">
+        <input type="submit" id="loginSubmit" value="Login">
     </div>
 </div>
 <link rel="stylesheet" type="text/css" href="../css/signIn.css">
--- Profil/register.php
+++ Profil/register.php
@@ -2,37 +2,27 @@
 
 <body class="app">
 <div class="register">
-    <div class="signIn">
-        <form>
-            <label id="Hregister">Registrieren</label>
-        </form>
+    <div class="registerElement">
+        <label id="Hregister">Registrieren</label>
     </div>
-    <div class="signIn">
-        <form>
-            <label id="rname">Name:</label>
-            <input type="text" id="rname">
-        </form>
+    <div class="registerElement">
+        <label id="rname">Name:</label>
+        <input type="text" id="rname">
     </div>
-    <div class="signIn">
-        <form>
-            <label id="remail">E-Mail Adresse:</label>
-            <input type="email" id="remail">
-        </form>
+    <div class="registerElement">
+        <label id="remail">E-Mail Adresse:</label>
+        <input type="email" id="remail">
     </div>
-    <div class="signIn">
-        <form>
-            <label id="rpassword">Passwort eingeben:</label>
-            <input type="password" id="rpassword">
-        </form>
+    <div class="registerElement">
+        <label id="rpassword">Passwort eingeben:</label>
+        <input type="password" id="rpassword">
     </div>
-    <div class="signIn">
-        <form>
-            <label id="repassword">Passwort wiederholen:</label>
-            <input type="password" id="repassword">
-        </form>
+    <div class="registerElement">
+        <label id="repassword">Passwort wiederholen:</label>
+        <input type="password" id="repassword">
     </div>
-    <div class="signIn">
-            <input type="submit" id="registerSubmit" value="Registrieren">
+    <div class="registerElement">
+        <input type="submit" id="registerSubmit" value="Registrieren">
     </div>
 </div>
 <link rel="stylesheet" type="text/css" href="../css/signIn.css">
