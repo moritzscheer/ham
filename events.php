45fb6d56f2ff89b30b7ed427d9e113a283ab33a4
Holger Dirks <h.dirks@28apps.de>
2023-05-09 23:51:49 +0200

    event view and band view

--- events.php
+++ events.php
@@ -1,43 +1,110 @@
 <?php include_once "php/head.php" ?>
 <body>
 <?php include_once "php/header.php" ?>
-<div>
-
-    <h1> Events</h1>
-
-    <!-- Event 1 -->
-    <div>
-        <img src="images/event.jpg" alt="eventImage"/>
-        <br>
-        <span>
-           Name: Neueröffnung Hornbach Bau- und Gartenmarkt Leipzig
-        </span>
-        <br>
-        <span>
-             Datum: 12 April 2023
-        </span>
-        <br>
-        <span>
-           Adresse: Steinweg 2, 42275 Wuppertal
-        </span>
+<div id="bands">
+    <div id="item-list-head">
+        <h2>Events</h2>
+        <div class="flexbox-center">
+            <label class="filter-box">
+                <span class="flexbox-center filter-label">Filter</span>
+                <input class="filter" type="text" name="filter">
+            </label>
+        </div>
     </div>
-    <br>
 
-    <!-- Event 2 -->
-    <div>
-        <img src="images/event2.jpg" alt="eventImage"/>
-        <br>
-        <span>
-           Name: MyWellness - Wellness-Center
-        </span>
-        <br>
-        <span>
-             Datum 22 Februar 2023
-        </span>
-        <br>
-        <span>
-           Adresse: Richard Lehmann Straße 117, 04103 Leipzig
-        </span>
+
+    <!-- Band 1 -->
+    <div id="item-list-view">
+        <section id="item-list">
+            <div id="item">
+                <img id="item-image" src="images/event.jpg" alt="eventImage"/>
+                <div id="item-description">
+                   <span>
+                       Name: Neueröffnung Hornbach Bau- und Gartenmarkt Leipzig
+                   </span>
+                    <br>
+                    <span>
+                        Datum: 12 April 2023
+                    </span>
+                    <br>
+                    <span>
+                        Adresse: Steinweg 2, 42275 Wuppertal
+                    </span>
+                </div>
+            </div>
+            <div id="item">
+                <img id="item-image" src="images/event2.jpg" alt="eventImage"/>
+                <div id="item-description">
+                     <span>
+                         Name: MyWellness - Wellness-Center
+                     </span>
+                    <br>
+                    <span>
+                        Datum 22 Februar 2023
+                    </span>
+                    <br>
+                    <span>
+                        Adresse: Richard Lehmann Straße 117, 04103 Leipzig
+                    </span>
+                </div>
+            </div>
+            <div id="item">
+                <img id="item-image" src="images/event.jpg" alt="eventImage"/>
+                <div id="item-description">
+                   <span>
+                       Name: Neueröffnung Hornbach Bau- und Gartenmarkt Leipzig
+                   </span>
+                    <br>
+                    <span>
+                        Datum: 12 April 2023
+                    </span>
+                    <br>
+                    <span>
+                        Adresse: Steinweg 2, 42275 Wuppertal
+                    </span>
+                </div>
+            </div>
+            <div id="item">
+                <img id="item-image" src="images/event2.jpg" alt="eventImage"/>
+                <div id="item-description">
+                     <span>
+                         Name: MyWellness - Wellness-Center
+                     </span>
+                    <br>
+                    <span>
+                        Datum 22 Februar 2023
+                    </span>
+                    <br>
+                    <span>
+                        Adresse: Richard Lehmann Straße 117, 04103 Leipzig
+                    </span>
+                </div>
+            </div>
+        </section>
+        <section id="item-details">
+            <div id="item-details-title">
+                <img id="item-image" src="images/event.jpg" alt="eventImage"/>
+                <h2 id="item-details-name"> Neueröffnung Hornbach Bau- und Gartenmarkt Leipzig </h2>
+            </div>
+            <div>
+                <p>
+                    Hornbach Bau- und Gartenmarkt eröffnet voraussichtlich im Frühjahr 2023 einen weiteren Markt in Leipzig auf rund 10.000 Quadratmetern und einem Gartenmarkt mit rund 3.000 Quadratmetern.
+                    <br>
+                    <br>
+                    Wann?: 22 Februar 2023 14-17 Uhr<br>
+                    Wo?: Richard Lehmann Straße 117, 04103 Leipzig<br>
+                </p>
+            </div>
+            <div id="item-details-foot">
+                <p>
+                    <b>Sucht </b> <br>
+                    <br>
+                    Jazzband mit Repertoir für einen Nachmittag (3std)<br>
+                    Kostenlose Verpflegung<br>
+                    Bis 2000€
+                </p>
+            </div>
+        </section>
     </div>
 </div>
 
