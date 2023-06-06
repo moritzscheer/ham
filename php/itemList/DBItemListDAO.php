9143ad592416f2a55a8fc54dac3127bde43aa37d
artur-S213 <mcdi1595@gmail.com>
2023-06-06 23:04:51 +0200

    Created DBItemListDAO.php class

---
+++ php/itemList/DBItemListDAO.php
@@ -0,0 +1,23 @@
+<?php
+
+class DBItemListDAO implements ItemListDAO
+{
+    public function __construct()
+    {
+        //todo: implement constructor method
+    }
+
+    public function loadItems($type): array
+    {
+        // TODO: Implement loadItems() method.
+        return array();
+    }
+
+    public function storeItem(Item $item): bool
+    {
+        // TODO: Implement storeItem() method.
+        return true;
+    }
+
+
+}
