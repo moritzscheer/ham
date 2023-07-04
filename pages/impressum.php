a9b3b16c61756eeeced42581daefb4ca6809184e
artur-S213 <mcdi1595@gmail.com>
2023-07-04 16:35:55 +0200

    formated impressum.php, datenschutz.php, nutzungsbedingungen.php

--- pages/impressum.php
+++ pages/impressum.php
@@ -2,46 +2,50 @@
 
 <body>
 
-<h1>Impressum</h1>
+<?php include_once "../php/includes/navigation/header/header.php" ?>
 
-<h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
-<p>ham<br/>
-    Ammerl&auml;nder Heerstra&szlig;e 114-118<br/>
-    26129 26129 Oldenburg</p>
+<section>
+    <h1>Impressum</h1>
 
-<p><strong>Vertreten durch:</strong><br/>
-    Moritz Scheer, Artur Scheling, (Holger Dirks)</p>
+    <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+    <p>ham<br/>
+        Ammerl&auml;nder Heerstra&szlig;e 114-118<br/>
+        26129 26129 Oldenburg</p>
 
-<p><strong>Vorsitzender des Aufsichtsrats:</strong><br/>
-    Moritz Scheer, Artur Scheling, (Holger Dirks)</p>
+    <p><strong>Vertreten durch:</strong><br/>
+        Moritz Scheer, Artur Scheling, (Holger Dirks)</p>
 
-<h2>Kontakt</h2>
-<p>Telefon: +49 441 798-0<br/>
-    Telefax: +49 441 798-3000<br/>
-    E-Mail: studium@uol.de</p>
+    <p><strong>Vorsitzender des Aufsichtsrats:</strong><br/>
+        Moritz Scheer, Artur Scheling, (Holger Dirks)</p>
 
-<h2>Umsatzsteuer-ID</h2>
-<p>Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:<br/>
-    DE999999999</p>
+    <h2>Kontakt</h2>
+    <p>Telefon: +49 441 798-0<br/>
+        Telefax: +49 441 798-3000<br/>
+        E-Mail: studium@uol.de</p>
 
-<h2>Redaktionell verantwortlich</h2>
-<p>Artur Scheling<br/>
-    Bookhorner Weg 18<br/>
-    27777 Ganderkesee</p>
+    <h2>Umsatzsteuer-ID</h2>
+    <p>Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:<br/>
+        DE999999999</p>
 
-<h2>EU-Streitschlichtung</h2>
-<p>Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a
-            href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br/>
-    Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
+    <h2>Redaktionell verantwortlich</h2>
+    <p>Artur Scheling<br/>
+        Bookhorner Weg 18<br/>
+        27777 Ganderkesee</p>
 
-<h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
-<p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle
-    teilzunehmen.</p>
+    <h2>EU-Streitschlichtung</h2>
+    <p>Die Europ&auml;ische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit: <a
+                href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br/>
+        Unsere E-Mail-Adresse finden Sie oben im Impressum.</p>
 
-<p>Quelle: <a
-            href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a>
-</p>
+    <h2>Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle</h2>
+    <p>Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle
+        teilzunehmen.</p>
 
+    <p>Quelle: <a
+                href="https://www.e-recht24.de/impressum-generator.html">https://www.e-recht24.de/impressum-generator.html</a>
+    </p>
+
+</section>
 </body>
 
 
--- pages/nutzungsbedingungen.php
+++ pages/nutzungsbedingungen.php
@@ -2,100 +2,118 @@
 
 <body>
 
-<p>&nbsp;</p><h4><span
-            style="text-decoration: underline;">Besondere Nutzungsbedingungen fÃ¼r die Forenregistrierung im </span><span
-            style="text-decoration: underline;">(Name Ihrer Domain)</span><span style="text-decoration: underline;"> -Forum:</span>
-</h4>
-<p>&nbsp;</p>
-<p><strong>§ 1 Geltungsbereich</strong></p>
-<p>(1)&nbsp;Die nachfolgenden Bedingungen gelten fÃ¼r die Nutzung des Name Ihrer Domain - im folgenden "Unsere Webseite"
-    genannt - Forums.&nbsp;FÃ¼r die Nutzung des Forums ist wichtig, dass Sie als Nutzer/Nutzerin die nachfolgenden
-    Forenregeln bzw. â€“bedingungen akzeptieren. Die Registrierung und Benutzung unseres Forums ist kostenlos.</p>
-<p>(2)&nbsp;Mit der Registrierung sind Sie mit den Nutzungsbedingungen unserer Webseite einverstanden. Durch Ihr
-    EinverstÃ¤ndnis garantieren Sie uns, dass Sie keine BeitrÃ¤ge erstellen werden, die gegen die Nutzungsbedingung
-    verstoÃŸen.</p>
-<p>(3) Durch das Benutzen von unserer Webseite kommt kein Vertrag zwischen den Nutzern/Nutzerinnen und uns zustande.</p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>§ 2 Pflichten als Forum-Nutzer/Forum-Nutzerinnen</strong></p>
-<p><sup>&nbsp;</sup>(1) Einer Ihrer Pflichten als Nutzer/Nutzerin ist es, dass Sie keine BeitrÃ¤ge veröffentlichen, die
-    gegen diese Forenregeln, gegen die guten Sitten oder sonst gegen geltendes deutsches Recht verstoßen.</p>
-<p><span style="text-decoration: underline;">Folgende Punkte sind Ihnen nicht genehmigt:</span></p>
-<p>1. Inhalte zu veröffentlichen, die unwahr sind und deren Veröffentlichung einen Straftatbestand oder eine
-    Ordnungswidrigkeit erfüllt,<br/>2. Versendung von Spam über das Forum an andere Forum-Nutzer/Form-Nutzerin,<br/>3.
-    Verwendung von gesetzlich durch Urheber- und Markenrecht geschätzte Inhalte ohne rechtmäßige Berechtigung (z.B.
-    Pressemitteilungen etc.),<br/>4. Handlungen, die wettbewerbswidrig sind,<br/>5. mehrfache Einstellung von Themen ins
-    Forum (sogenannte Doppelpostings),<br/>6. eigene Werbung, folglich Schleichwerbung, zu betreiben und<br/>7. Inhalte
-    zu verÃ¶ffentlichen, die beleidigend, rassistisch, diskriminierend oder pornographische Elemente aufweisen
-    gegenÃ¼ber anderen Nutzern/Nutzerinnen und der Allgemeinheit.</p>
-<p>Ihre Pflicht als Forum-Nutzer/ Forum-Nutzerin ist es, auf § 2 Abs. 1 Nr. 1-7 dieser Nutzungsbedingung, vor&nbsp; der
-    Veröffentlichung Ihres Beitrages im Forum zu beachten und zu überprüfen, ob Sie sich an diese Punkte gehalten
-    haben.</p>
-<p>(2)&nbsp;&nbsp;&nbsp;Sollten Sie gegen § 2 Abs. 1 Nr. 1-7 dieser Nutzungsbedingung verstoßen, behalten wir uns das
-    Recht vor, gegen Sie folgende Schritte vorzunehmen:</p>
-<p>1. Ihre eingestellten BeitrÃ¤ge zu löschen und diese abzuÃ¤ndern,</p>
-<p>2. Verbot weiterhin im Forum Beiträge zu verfassen und</p>
-<p>3. Sperrung des Zugangs als Nutzer/Nutzerin.</p>
-<p>(3)&nbsp;&nbsp;Haben Sie als Forum-Nutzer/Forum-Nutzerin nicht die Forenregeln beachtet und sind hierdurch mögliche
-    Rechtsverstöße entstanden, die durch Ihre eingestellten Inhalte in unserem Forum entstanden sind
-    (Pflichtverletzung), verpflichten Sie sich als Nutzer/Nutzerin, <em>uns </em>von jeglichen Ansprüchen, auch den
-    Schadenersatzansprüchen, freizustellen und diesen die Kosten zu ersetzen.</p>
-<p>Zudem ist der Nutzer/die Nutzerin verpflichtet uns, bei Schadenersatzansprüchen hinsichtlich der Abwehr des durch
-    ihn entstandenen Rechtsverstoßes (Pflichtverletzung s.o.), zu unterstützen und die Kosten einer angemessenen
-    Rechtsverteidigung für uns zu tragen.</p>
-<p>(4)&nbsp;&nbsp; Durch Ihr Einverständnis garantieren Sie uns, dass Sie keine BetrÃ¤ge erstellen werden, die gegen
-    die Nutzungsbedingung verstoÃŸen. Entsprechendes gilt auch für das Setzen von externen Links und die
-    Signaturen.</p>
-<p>&nbsp;</p>
-<p><strong>§ 3 Übertragung von Nutzungsrechten</strong></p>
-<p>Sie, als Forum-Nutzer/Forum-Nutzerin&nbsp; haben die alleinige Verantwortung des Urheberrechts i.S.d. Urhebergesetzes
-    bei VerÃ¶ffentlichung von BeitrÃ¤gen und Themen im Forum zu beachten. <sup>2</sup>Als Nutzer/Nutzerin räumen Sie
-    lediglich uns mit VerÃ¶ffentlichung Ihres Beitrages auf deren Homepage das Recht ein, den Beitrag dauerhaft zum
-    Abruf bereitzustellen. <sup>3</sup>Ferner hat unser Team das Recht, Ihre Themen und BeitrÃ¤ge zu löschen, zu
-    bearbeiten und innerhalb seiner Homepage zu verschieben, um diese mit anderen Inhalten zu verknüpfen oder zu
-    schließen.</p>
-<p>&nbsp;</p>
-<p><strong>§ 4 Haftungsbeschränkung</strong></p>
-<p>(1) Wir übernehmen keinerlei Gewähr für die im Forum veröffentlichten und eingestellten BeitrÃ¤ge, Themen,
-    externen Links und die daraus resultierenden Inhalte, insbesondere nicht für deren Richtigkeit, Vollständigkeit
-    und Aktualität. Wir sind auch nicht verpflichtet, permanent die Übermittelten und gespeicherten Beiträge der
-    Nutzer/Nutzerinnen zu überwachen oder nach den Umständen zu erforschen, ob sie auf einen rechtswidrigen Inhalt
-    hinweisen. Wir haften grundsätzlich nur im Falle einer vorsätzlichen oder grob fahrlässigen
-    Pflichtverletzung.</p>
-<p>(2) WIr weisen ausdrücklich darauf hin, dass die juristischen BeitrÃ¤ge und Diskussionen im Forum vollkommen
-    unverbindlich sind. <sup>2</sup>Die Nutzung der Beiträge und deren Verwertung erfolgt auf Ihre eigene Gefahr.</p>
-<p>(3)&nbsp;&nbsp; Bei Werbeschaltungen Ã¼bernimmen wir keine Haftung für den Inhalt und die Richtigkeit. <sup>2 </sup>Für
-    den Inhalt der Werbeanzeigen ist der jeweilige Autor einzig und allein verantwortlich; gleiches gilt für den Inhalt
-    der beworbenen Webseite. <sup>3</sup>Bei Darstellung der Werbeanzeige auf unserer Webseite , sind wir nicht
-    gleichzeitig mit dem rechtswidrigen Inhalt einverstanden. <sup>4</sup>Daher liegt die Haftung ausschließlich bei
-    dem Werbekunden.</p>
-<p>(4)&nbsp;&nbsp; Es ist wird nicht für einen ständigen unterbrechungsfreien Abruf der Webseite garantiert.
-    <sup>2</sup>Einer Haftung diesbezüglich wird hiermit ausdrücklich widersprochen. <sup>3</sup>Auch bei großer
-    Sorgfalt können Ausfallzeiten leider nicht ausgeschlossen werden.</p>
-<p>&nbsp;</p>
-<p><strong>§ 5 Urheberrecht</strong></p>
-<p>Sämtliche Texte, Bilder und andere auf unserer Webseite veröffentlichten Informationen und Daten unterliegen -
-    sofern nicht anders gekennzeichnet - dem Copyright unserer Seite. Jede Form von Wiedergabe und/oder Modifikation
-    darf nur mit der schriftlichen Genehmigung durch uns erfolgen. Andersfalls behalten wir uns das Recht vor
-    gerichtlich gegen diese Rechtsverletzung vorzugehen. <sup>4</sup>Alle Kosten, die durch eine Rechtsverletzung
-    seitens eines Benutzers verursacht werden, werden diesem in Rechnung gestellt.</p>
-<p><strong>&nbsp;</strong></p>
-<p><strong>§ 6 Änderungsvorbehalt</strong></p>
-<p>Wir haben jederzeit das Recht die Nutzungsbedingungen zu ändern. Die Änderung wird dann per Forum-Eintrag auf der
-    Webseite veröffentlicht.</p>
-<p>&nbsp;</p>
-<p><strong>§ 7 Kündigung und Laufzeit der Mitgliedschaft bei unserer Webseite <br/></strong></p>
-<p>Die Laufzeit der Mitgliedschaft beginnt mit der Registrierung und mit dem Einverständnis unseren Nutzungsbedingungen
-    und besteht auf eine unbestimmte Zeit. Die Mitgliedschaft kann jederzeit ohne Angabe von Kündigen fristlos
-    gekündigt werden.</p>
-<p>&nbsp;</p>
-<p><strong>§ 8 Salvatorische Klausel</strong></p>
-<p>Diese Forum-Nutzungsbedingung ist als Teil unserer Webseite zu betrachten, von dem aus auf diese Seite verwiesen
-    wird. Sind einzelne Formulierungen dieser Forum-Nutzungsbedingung nicht mehr ganz oder nicht mehr vollständig
-    konform mit der geltenden Rechtslage, so ist davon auszugehen, dass die übrigen Regelungen der
-    Forum-Nutzungsbedingungen bestehen bleiben.</p>
-<p>Diese <a href="http://www.jurarat.de/muster-nutzungsbedingungen">Nutzungsbedingungen</a> wurden freundlicherweise von
-    www.jurarat.de zur Verfügung gestellt.
+<?php include_once "../php/includes/navigation/header/header.php" ?>
 
+<section>
+    <p>&nbsp;</p><h4><span
+                style="text-decoration: underline;">Besondere Nutzungsbedingungen fÃ¼r die Forenregistrierung im </span><span
+                style="text-decoration: underline;">(Name Ihrer Domain)</span><span style="text-decoration: underline;"> -Forum:</span>
+    </h4>
+    <p>&nbsp;</p>
+    <p><strong>§ 1 Geltungsbereich</strong></p>
+    <p>(1)&nbsp;Die nachfolgenden Bedingungen gelten fÃ¼r die Nutzung des Name Ihrer Domain - im folgenden "Unsere
+        Webseite"
+        genannt - Forums.&nbsp;FÃ¼r die Nutzung des Forums ist wichtig, dass Sie als Nutzer/Nutzerin die nachfolgenden
+        Forenregeln bzw. â€“bedingungen akzeptieren. Die Registrierung und Benutzung unseres Forums ist kostenlos.</p>
+    <p>(2)&nbsp;Mit der Registrierung sind Sie mit den Nutzungsbedingungen unserer Webseite einverstanden. Durch Ihr
+        EinverstÃ¤ndnis garantieren Sie uns, dass Sie keine BeitrÃ¤ge erstellen werden, die gegen die Nutzungsbedingung
+        verstoÃŸen.</p>
+    <p>(3) Durch das Benutzen von unserer Webseite kommt kein Vertrag zwischen den Nutzern/Nutzerinnen und uns
+        zustande.</p>
+    <p><strong>&nbsp;</strong></p>
+    <p><strong>§ 2 Pflichten als Forum-Nutzer/Forum-Nutzerinnen</strong></p>
+    <p><sup>&nbsp;</sup>(1) Einer Ihrer Pflichten als Nutzer/Nutzerin ist es, dass Sie keine BeitrÃ¤ge veröffentlichen,
+        die
+        gegen diese Forenregeln, gegen die guten Sitten oder sonst gegen geltendes deutsches Recht verstoßen.</p>
+    <p><span style="text-decoration: underline;">Folgende Punkte sind Ihnen nicht genehmigt:</span></p>
+    <p>1. Inhalte zu veröffentlichen, die unwahr sind und deren Veröffentlichung einen Straftatbestand oder eine
+        Ordnungswidrigkeit erfüllt,<br/>2. Versendung von Spam über das Forum an andere Forum-Nutzer/Form-Nutzerin,<br/>3.
+        Verwendung von gesetzlich durch Urheber- und Markenrecht geschätzte Inhalte ohne rechtmäßige Berechtigung (z.B.
+        Pressemitteilungen etc.),<br/>4. Handlungen, die wettbewerbswidrig sind,<br/>5. mehrfache Einstellung von Themen
+        ins
+        Forum (sogenannte Doppelpostings),<br/>6. eigene Werbung, folglich Schleichwerbung, zu betreiben und<br/>7.
+        Inhalte
+        zu verÃ¶ffentlichen, die beleidigend, rassistisch, diskriminierend oder pornographische Elemente aufweisen
+        gegenÃ¼ber anderen Nutzern/Nutzerinnen und der Allgemeinheit.</p>
+    <p>Ihre Pflicht als Forum-Nutzer/ Forum-Nutzerin ist es, auf § 2 Abs. 1 Nr. 1-7 dieser Nutzungsbedingung, vor&nbsp;
+        der
+        Veröffentlichung Ihres Beitrages im Forum zu beachten und zu überprüfen, ob Sie sich an diese Punkte gehalten
+        haben.</p>
+    <p>(2)&nbsp;&nbsp;&nbsp;Sollten Sie gegen § 2 Abs. 1 Nr. 1-7 dieser Nutzungsbedingung verstoßen, behalten wir uns
+        das
+        Recht vor, gegen Sie folgende Schritte vorzunehmen:</p>
+    <p>1. Ihre eingestellten BeitrÃ¤ge zu löschen und diese abzuÃ¤ndern,</p>
+    <p>2. Verbot weiterhin im Forum Beiträge zu verfassen und</p>
+    <p>3. Sperrung des Zugangs als Nutzer/Nutzerin.</p>
+    <p>(3)&nbsp;&nbsp;Haben Sie als Forum-Nutzer/Forum-Nutzerin nicht die Forenregeln beachtet und sind hierdurch
+        mögliche
+        Rechtsverstöße entstanden, die durch Ihre eingestellten Inhalte in unserem Forum entstanden sind
+        (Pflichtverletzung), verpflichten Sie sich als Nutzer/Nutzerin, <em>uns </em>von jeglichen Ansprüchen, auch den
+        Schadenersatzansprüchen, freizustellen und diesen die Kosten zu ersetzen.</p>
+    <p>Zudem ist der Nutzer/die Nutzerin verpflichtet uns, bei Schadenersatzansprüchen hinsichtlich der Abwehr des durch
+        ihn entstandenen Rechtsverstoßes (Pflichtverletzung s.o.), zu unterstützen und die Kosten einer angemessenen
+        Rechtsverteidigung für uns zu tragen.</p>
+    <p>(4)&nbsp;&nbsp; Durch Ihr Einverständnis garantieren Sie uns, dass Sie keine BetrÃ¤ge erstellen werden, die gegen
+        die Nutzungsbedingung verstoÃŸen. Entsprechendes gilt auch für das Setzen von externen Links und die
+        Signaturen.</p>
+    <p>&nbsp;</p>
+    <p><strong>§ 3 Übertragung von Nutzungsrechten</strong></p>
+    <p>Sie, als Forum-Nutzer/Forum-Nutzerin&nbsp; haben die alleinige Verantwortung des Urheberrechts i.S.d.
+        Urhebergesetzes
+        bei VerÃ¶ffentlichung von BeitrÃ¤gen und Themen im Forum zu beachten. <sup>2</sup>Als Nutzer/Nutzerin räumen Sie
+        lediglich uns mit VerÃ¶ffentlichung Ihres Beitrages auf deren Homepage das Recht ein, den Beitrag dauerhaft zum
+        Abruf bereitzustellen. <sup>3</sup>Ferner hat unser Team das Recht, Ihre Themen und BeitrÃ¤ge zu löschen, zu
+        bearbeiten und innerhalb seiner Homepage zu verschieben, um diese mit anderen Inhalten zu verknüpfen oder zu
+        schließen.</p>
+    <p>&nbsp;</p>
+    <p><strong>§ 4 Haftungsbeschränkung</strong></p>
+    <p>(1) Wir übernehmen keinerlei Gewähr für die im Forum veröffentlichten und eingestellten BeitrÃ¤ge, Themen,
+        externen Links und die daraus resultierenden Inhalte, insbesondere nicht für deren Richtigkeit, Vollständigkeit
+        und Aktualität. Wir sind auch nicht verpflichtet, permanent die Übermittelten und gespeicherten Beiträge der
+        Nutzer/Nutzerinnen zu überwachen oder nach den Umständen zu erforschen, ob sie auf einen rechtswidrigen Inhalt
+        hinweisen. Wir haften grundsätzlich nur im Falle einer vorsätzlichen oder grob fahrlässigen
+        Pflichtverletzung.</p>
+    <p>(2) WIr weisen ausdrücklich darauf hin, dass die juristischen BeitrÃ¤ge und Diskussionen im Forum vollkommen
+        unverbindlich sind. <sup>2</sup>Die Nutzung der Beiträge und deren Verwertung erfolgt auf Ihre eigene Gefahr.
+    </p>
+    <p>(3)&nbsp;&nbsp; Bei Werbeschaltungen Ã¼bernimmen wir keine Haftung für den Inhalt und die Richtigkeit.
+        <sup>2 </sup>Für
+        den Inhalt der Werbeanzeigen ist der jeweilige Autor einzig und allein verantwortlich; gleiches gilt für den
+        Inhalt
+        der beworbenen Webseite. <sup>3</sup>Bei Darstellung der Werbeanzeige auf unserer Webseite , sind wir nicht
+        gleichzeitig mit dem rechtswidrigen Inhalt einverstanden. <sup>4</sup>Daher liegt die Haftung ausschließlich bei
+        dem Werbekunden.</p>
+    <p>(4)&nbsp;&nbsp; Es ist wird nicht für einen ständigen unterbrechungsfreien Abruf der Webseite garantiert.
+        <sup>2</sup>Einer Haftung diesbezüglich wird hiermit ausdrücklich widersprochen. <sup>3</sup>Auch bei großer
+        Sorgfalt können Ausfallzeiten leider nicht ausgeschlossen werden.</p>
+    <p>&nbsp;</p>
+    <p><strong>§ 5 Urheberrecht</strong></p>
+    <p>Sämtliche Texte, Bilder und andere auf unserer Webseite veröffentlichten Informationen und Daten unterliegen -
+        sofern nicht anders gekennzeichnet - dem Copyright unserer Seite. Jede Form von Wiedergabe und/oder Modifikation
+        darf nur mit der schriftlichen Genehmigung durch uns erfolgen. Andersfalls behalten wir uns das Recht vor
+        gerichtlich gegen diese Rechtsverletzung vorzugehen. <sup>4</sup>Alle Kosten, die durch eine Rechtsverletzung
+        seitens eines Benutzers verursacht werden, werden diesem in Rechnung gestellt.</p>
+    <p><strong>&nbsp;</strong></p>
+    <p><strong>§ 6 Änderungsvorbehalt</strong></p>
+    <p>Wir haben jederzeit das Recht die Nutzungsbedingungen zu ändern. Die Änderung wird dann per Forum-Eintrag auf der
+        Webseite veröffentlicht.</p>
+    <p>&nbsp;</p>
+    <p><strong>§ 7 Kündigung und Laufzeit der Mitgliedschaft bei unserer Webseite <br/></strong></p>
+    <p>Die Laufzeit der Mitgliedschaft beginnt mit der Registrierung und mit dem Einverständnis unseren
+        Nutzungsbedingungen
+        und besteht auf eine unbestimmte Zeit. Die Mitgliedschaft kann jederzeit ohne Angabe von Kündigen fristlos
+        gekündigt werden.</p>
+    <p>&nbsp;</p>
+    <p><strong>§ 8 Salvatorische Klausel</strong></p>
+    <p>Diese Forum-Nutzungsbedingung ist als Teil unserer Webseite zu betrachten, von dem aus auf diese Seite verwiesen
+        wird. Sind einzelne Formulierungen dieser Forum-Nutzungsbedingung nicht mehr ganz oder nicht mehr vollständig
+        konform mit der geltenden Rechtslage, so ist davon auszugehen, dass die übrigen Regelungen der
+        Forum-Nutzungsbedingungen bestehen bleiben.</p>
+    <p>Diese <a href="http://www.jurarat.de/muster-nutzungsbedingungen">Nutzungsbedingungen</a> wurden freundlicherweise
+        von
+        www.jurarat.de zur Verfügung gestellt.
+
+</section>
 
 </body>
 
